e032ed34697565a846d6534b6abcb5bf8a3223be
Mikhailyuk Sergey <vzxyqw@gmail.com>
2020-09-28 12:49:54 +0300

    done work

---
+++ task-3.js
@@ -0,0 +1,31 @@
+// Write code under this line
+
+// console.log(typeof Storage);
+// 'function'
+
+const goods = ["Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор"];
+
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    this.items.push(item);
+  }
+  removeItem(item) {
+    let index = this.items.indexOf(item);
+    this.items.splice(index, 1);
+  }
+}
+const storage = new Storage(goods);
+
+console.log(storage.getItems()); //['Нанитоиды','Пролонгер','Железные жупи','Антигравитатор']
+
+storage.addItem("Дроид");
+console.log(storage.getItems()); //["Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид"];
+
+storage.removeItem("Пролонгер");
+console.log(storage.getItems()); //["Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид"];
